42bf7fa42a911f884bcce8bccd848033cffd244e
Barbara Engle <barbara.engle@outlook.com>
2020-03-19 10:26:15 +0200

    Add project files.

---
+++ AppFactory2020/Model/Intern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppFactory2020.Model
+{
+    public class Intern
+    {
+        public int MyPersonId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string CellPhoneNo { get; set; }
+    }
+}
---
+++ AppFactory2020/Model/InternList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppFactory2020.Model
+{
+    public class InternList
+    {
+        public List<Intern> Interns { get; set; }
+    }
+}
